5d6ea64f40779521fc297a0810bb21d5f8c59264
kitaz42 <kutajartem550@gmail.com>
2026-05-02 20:37:17 +0300

    feat: add kitty and waybar configs

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DOT_DIR=$(pwd)
 ln -sf "$DOT_DIR/hypr" ~/.config/
+ln -sf "$DOT_DIR/kitty" ~/.config/
+ln -sf "$DOT_DIR/waybar" ~/.config/
 ln -sf "$DOT_DIR/.zshrc" ~/.zshrc
 echo "Конфиги связаны."
