ed3a82ab299cf22498427d41c2a4c232c28afbc4
kitaz42 <kutajartem550@gmail.com>
2026-05-02 22:14:29 +0300

    install.sh full uprade

--- install.sh
+++ install.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+PACKAGES=(
+	 hyprland
+    waybar
+    kitty
+    zsh
+    swww
+    fastfetch
+    nano
+    git 
+)
+echo "Установка софта..."
+
+	for pkg in "${PACKAGES[@]}"; do
+	if pacman -Qi "$pkg" &> /dev/null; then
+        echo "$pkg уже установлен"
+    else
+        echo "Устанавливаю $pkg..."
+        sudo pacman -S --noconfirm "$pkg"
+    fi
+done
+
 DOT_DIR=$(pwd)
 ln -sf "$DOT_DIR/hypr" ~/.config/
 ln -sf "$DOT_DIR/kitty" ~/.config/
